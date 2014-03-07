b029f9f6d390b9fb646b3c73e70deb15ea6d56e8
boyang070822 <boyang070822@gmail.com>
2014-03-07 15:09:06 +0800

    Test

---
+++ service/src/test/java/com/elex/bigdata/user_category/service/TestServerServlet.java
@@ -0,0 +1,18 @@
+package com.elex.bigdata.user_category.service;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yb
+ * Date: 3/7/14
+ * Time: 2:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestServerServlet {
+  @Test
+  public void test(){
+    QueryServerServlet serverServlet=new QueryServerServlet();
+    System.out.println("hh");
+  }
+}
